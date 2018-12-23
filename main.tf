01e6fc67213d70aae0f5143bf4f82651d280e1e1
Ted <tedmart92@gmail.com>
2018-12-22 22:22:58 -0600

    commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+##############################################################################
+# * Beginner's Guide to Using Terraform on Azure
+# 
+# This Terraform configuration will create the following:
+#
+# Resource group with a virtual network and subnet
+# An Windows Server Running IIS
+
+
