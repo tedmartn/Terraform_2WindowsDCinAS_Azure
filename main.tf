64b80b4907af4ef9a464ae29a380b1471b20d5e5
redvelvet92 <tedmart92@gmail.com>
2018-12-22 22:26:21 -0600

    commit\

--- main.tf
+++ main.tf
@@ -6,4 +6,9 @@
 # Resource group with a virtual network and subnet
 # An Windows Server Running IIS
 
+resource "azurerm_resource_group" "test_terraform_rg" {
+    name = "${var.resource.group}"
+    location "${var.location}"
+}
+
 
